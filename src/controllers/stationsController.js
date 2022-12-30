2f5015ef08a58f4b0342a965df25175d60df3113
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-12-30 20:37:13 +0200

    edited getStations function

--- src/controllers/stationsController.js
+++ src/controllers/stationsController.js
@@ -1,28 +1,55 @@
-const Station=require("../models/stationModel")
+const Station = require("../models/stationModel");
 
-const getStations=async(req,res)=>{
-    try {
-        const tasks = await Station.find().limit(15);
-        res.status(200).json(tasks);
-      } catch (error) {
-        res.status(500).json({ msg: error.message });
-      }
-}
+const getStations = async (req, res) => {
+  console.log("got request")
+  try {
+    const limit = parseInt(req.query.limit) || 15;
+    const page = parseInt(req.query.page) || 0;
+    const search = req.query.search;
+    const searchType = req.query.searchType;
+    const offset = limit * page;
 
-const getStationOccurences=async(req,res)=>{
-    const station_id=req.params.id;
-    try {
-        const departureCounts = await Station.find({id:[station_id]}).count();
-        res.status(200).json(tasks);
-      } catch (error) {
-        res.status(500).json({ msg: error.message });
-      }
+    /*
 
-}
+    const tasks = await Station.find().limit(15);
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+  */
+
+    let stationsCollection;
+    let stationsCollectionCount;
+
+    if (search) {
+      stationsCollection = await Station.find({
+        [searchType]: { $regex: search, $options: "i" },
+      })
+        .skip(offset)
+        .limit(limit);
+      stationsCollectionCount = await Station.count({
+        [searchType]: { $regex: search, $options: "i" },
+      });
+    } else {
+      stationsCollection = await Station.find().skip(offset).limit(limit);
+      stationsCollectionCount = await Station.count();
+    }
+
+    const totalPages = Math.ceil(stationsCollectionCount / limit);
+
+    res.status(200).send({
+      data: stationsCollection,
+      paging: {
+        total: stationsCollectionCount,
+        page: page,
+        numberOfPages: totalPages,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
 
 module.exports = {
-    getStations,
-  };
-
-
-
+  getStations,
+};
--- src/controllers/tripController.js
+++ src/controllers/tripController.js
@@ -14,21 +14,17 @@ const getTrips = async (req, res) => {
 
     if (search) {
       tripsCollection = await Trip.find({
-        [searchType]: { $regex: search, $options:'i'  },
+        [searchType]: { $regex: search, $options: "i" },
       })
         .skip(offset)
         .limit(limit);
       tripsCollectionCount = await Trip.count({
-        [searchType]: { $regex: search, $options:'i' },
+        [searchType]: { $regex: search, $options: "i" },
       });
-    } else{
+    } else {
       tripsCollection = await Trip.find().skip(offset).limit(limit);
       tripsCollectionCount = await Trip.count();
-
-
     }
-
-    //aggregate
 
 
     const totalPages = Math.ceil(tripsCollectionCount / limit);
@@ -46,22 +42,23 @@ const getTrips = async (req, res) => {
   }
 };
 
-
-const getStationOccurences=async(req,res)=>{
-  console.log(req.params.id)
-  const station_id=req.params.id;
+const getStationOccurences = async (req, res) => {
+  console.log(req.params.id);
+  const station_id = req.params.id;
   try {
-      const departureCounts = await Trip.find({departure_station_id:[station_id]}).count();
-      const returnCounts = await Trip.find({return_station_id:[station_id]}).count();
-      res.status(200).json({departureCounts,returnCounts});
-    } catch (error) {
-      res.status(500).json({ msg: error.message });
-    }
-
-}
-
+    const departureCounts = await Trip.find({
+      departure_station_id: [station_id],
+    }).count();
+    const returnCounts = await Trip.find({
+      return_station_id: [station_id],
+    }).count();
+    res.status(200).json({ departureCounts, returnCounts });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
 
 module.exports = {
   getTrips,
-  getStationOccurences
+  getStationOccurences,
 };
